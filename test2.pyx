c6c681ffa5b4bcc596b7d848658054949201c427
scott.lan <scott.lan@taisys.com>
2014-03-24 17:54:51 +0800

    增加一個遞迴展開目錄例子

--- test2.pyx
+++ test2.pyx
@@ -1,6 +1,27 @@
 # coding:utf-8
 
-import my_lib
+try:
+    import my_lib
+except Exception:
+    pass
 
 def run_for_test2(x):
     print x, 'i run in test2'
+
+# 遞迴展開目錄
+def scan_dir(path):
+    import glob
+    files = glob.glob(path+'/*')
+    if len(files) == 0:
+        return len(files)
+    else:
+        for i in files:
+            l = scan_dir(i)
+            print i
+
+
+        return len(files)
+
+if __name__ == '__main__':
+    scan_dir('.')
+    
